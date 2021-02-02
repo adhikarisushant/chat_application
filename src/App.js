ed74be25c81469ef7b2030b3bf4e95bee8ae82c0
Sushant <sushant.adhikari.619@gmail.com>
2021-02-02 19:31:54 +0545

    login functionality

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import { ChatEngine } from 'react-chat-engine';
 
+import LoginForm from './components/LoginForm';
 import ChatFeed from './components/ChatFeed';
 
 import './App.css'; 
 
 
 const App = () => {
+    if(!localStorage.getItem('username')) return <LoginForm />
+
     return (
         <ChatEngine 
             height="100vh"
             projectID="2824f75c-2925-40d4-9ab9-72c49da3d174"
-            userName="sagarLamsal"
-            userSecret="123123"
+            userName={localStorage.getItem('username')}
+            userSecret={localStorage.getItem('password')}
             renderChatFeed={(chatAppProps) => <ChatFeed { ...chatAppProps } />}
         />
     );
