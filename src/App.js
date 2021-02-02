f0163428fd83bce9127994ff3bfb89244fe96d50
Sushant <sushant.adhikari.619@gmail.com>
2021-02-02 11:31:10 +0545

    rendering messages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { ChatEngine } from 'react-chat-engine';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import ChatFeed from './components/ChatFeed';
+
+import './App.css'; 
+
+
+const App = () => {
+    return (
+        <ChatEngine 
+            height="100vh"
+            projectID="2824f75c-2925-40d4-9ab9-72c49da3d174"
+            userName="sushantAdhikari"
+            userSecret="123123"
+            renderChatFeed={(chatAppProps) => <ChatFeed { ...chatAppProps } />}
+        />
+    );
 }
 
 export default App;
